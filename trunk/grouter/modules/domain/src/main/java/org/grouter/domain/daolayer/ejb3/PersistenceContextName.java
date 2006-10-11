a14478f179b2fba297c2c23a80c4fec0b6ac61a7
gepo01 <gepo01@6e64f7dd-db05-0410-9ee2-d86f13d23bb4>
2006-10-11 10:36:07 +0000

    heavy refactoring

---
+++ trunk/grouter/modules/domain/src/main/java/org/grouter/domain/daolayer/ejb3/PersistenceContextName.java
@@ -0,0 +1,15 @@
+package org.grouter.domain.daolayer.ejb3;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: geopol
+ * Date: Sep 29, 2006
+ * Time: 2:57:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PersistenceContextName
+{
+    public final static String PERSISTENCE ="grouterDomain";
+
+    public final static String JNDI_ENTITYMANAGER = "java:/grouterDomain";
+}
---
+++ trunk/grouter/modules/domain/src/main/java/org/grouter/domain/daolayer/ejb3/SystemUserDAOBean.java
@@ -0,0 +1,60 @@
+package org.grouter.domain.daolayer.ejb3;
+
+import org.grouter.domain.entities.Message;
+import org.grouter.domain.entities.systemuser.SystemUser;
+import org.grouter.domain.daolayer.SystemUserDAO;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: geopol
+ * Date: Oct 9, 2006
+ * Time: 9:42:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SystemUserDAOBean extends GenericEjb3DAO<SystemUser, Long> implements SystemUserDAO
+{
+    public SystemUserDAOBean()
+    {
+        super(Message.class);
+    }
+
+    public SystemUserDAOBean(Class persistentClass)
+    {
+        super(persistentClass);
+    }
+
+    public SystemUser saveOrUpdate(SystemUser entity)
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void delete(SystemUser entity)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public SystemUser createSystemUser(SystemUser systemUser)
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void removeUser(Long id)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void removeUser(String userName)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public boolean userExists(String userName)
+    {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public boolean userExists(Long id)
+    {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
