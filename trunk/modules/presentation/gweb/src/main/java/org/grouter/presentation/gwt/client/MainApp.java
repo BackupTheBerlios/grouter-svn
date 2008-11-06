69183b292fdd1fe04419f737851d47e5671d14e8
gepo01 <gepo01@6e64f7dd-db05-0410-9ee2-d86f13d23bb4>
2008-11-06 07:14:50 +0000

    Support for GWT

--- trunk/modules/presentation/gweb/src/main/java/org/grouter/presentation/gwt/client/MainApp.java
+++ trunk/modules/presentation/gweb/src/main/java/org/grouter/presentation/gwt/client/MainApp.java
@@ -1,18 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.grouter.presentation.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.Window;
-
-import org.grouter.presentation.gwt.client.widget.MessageContainer;
+import com.gwtext.client.core.EventObject;
+import com.gwtext.client.core.Margins;
+import com.gwtext.client.core.RegionPosition;
+import com.gwtext.client.widgets.Button;
+import com.gwtext.client.widgets.Panel;
+import com.gwtext.client.widgets.TabPanel;
+import com.gwtext.client.widgets.Viewport;
+import com.gwtext.client.widgets.event.ButtonListenerAdapter;
+import com.gwtext.client.widgets.event.TabPanelListenerAdapter;
+import com.gwtext.client.widgets.layout.BorderLayout;
+import com.gwtext.client.widgets.layout.BorderLayoutData;
+import com.gwtext.client.widgets.layout.FitLayout;
+import com.gwtext.client.widgets.layout.VerticalLayout;
+import com.gwtext.client.widgets.tree.TreeNode;
+import com.gwtext.client.widgets.tree.TreePanel;
 
 
 /**
- * Created by IntelliJ IDEA.
- * User: georgespolyzois
- * Date: Dec 25, 2007
- * Time: 10:26:04 PM
- * To change this template use File | Settings | File Templates.
+ * Main entrypoint for starting.
+ *
+ * @author Georges Polyzois
  */
 
 public class MainApp implements EntryPoint, ApplicationStateEventListener
@@ -20,12 +47,11 @@ public class MainApp implements EntryPoint, ApplicationStateEventListener
     //final WidgetContainer messageContentPanel = MessageContainer.getInstance().getPanel();
     private static final String WIDTH = "100%";
     private static final String HEIGTH = "100%";
+
     /**
      * An instance of the constants.
      */
     //private CwConstants constants;
-
-
     public MainApp()
     {
         ApplicationEventHandler.getInstance().addApplicationStateEventListener(this);
@@ -131,8 +157,29 @@ public class MainApp implements EntryPoint, ApplicationStateEventListener
 
     public void onModuleLoad()
     {
+        Panel panel = new Panel();
+        panel.setBorder(false);
+        panel.setPaddings(15);
+        panel.setLayout(new FitLayout());
 
-        // Create a Dock Panel
+        Panel borderPanel = new Panel();
+        borderPanel.setLayout(new BorderLayout());
+
+        //add north panel
+        //createNortPanel(borderPanel);
+
+
+
+
+        createAndAddWestPanel(borderPanel);
+
+        createAndAddCenterPanel(borderPanel);
+
+        panel.add(borderPanel);
+
+        Viewport viewport = new Viewport(panel);
+
+        /* // Create a Dock Panel
         DockPanel dock = new DockPanel();
         dock.setStyleName("cw-DockPanel");
         dock.setSpacing(4);
@@ -193,7 +240,156 @@ public class MainApp implements EntryPoint, ApplicationStateEventListener
 
         // Add it to the root panel.
         RootPanel.get().add(dock);
+
+        */
     }
+
+    private void createAndAddCenterPanel(Panel borderPanel)
+    {
+        Panel panel = new Panel();
+        final TabPanel tabPanel = new TabPanel();
+
+         panel.setBorder(false);
+         panel.setPaddings(15);
+         Panel verticalPanel = new Panel();
+         verticalPanel.setLayout(new VerticalLayout(15));
+            Button button = new Button("Add Tab", new ButtonListenerAdapter() {
+             public void onClick(Button button, EventObject e) {
+                 Panel tab = addTab(tabPanel);
+                 tabPanel.activate(tab.getId());
+                 tabPanel.scrollToTab(tab, true);
+             }
+         });
+         button.setIconCls("new-tab-icon");
+         verticalPanel.add(button);
+
+
+         tabPanel.setResizeTabs(true);
+         tabPanel.setMinTabWidth(115);
+        tabPanel.setTabWidth(135);
+         tabPanel.setEnableTabScroll(true);
+         tabPanel.setWidth(450);  
+         tabPanel.setHeight(250);
+         tabPanel.setActiveTab(0);
+
+         tabPanel.addListener(new TabPanelListenerAdapter() {
+             public void onContextMenu(TabPanel source, Panel tab, EventObject e) {
+                 showMenu(tab, e);
+             }
+         });
+
+         for (int index = 0; index < 7; index++) {
+             addTab();
+         }
+         verticalPanel.add(tabPanel);
+         panel.add(verticalPanel);
+
+
+
+
+
+
+
+
+
+        borderPanel.add(panel, new BorderLayoutData(RegionPosition.CENTER));
+    }
+
+
+       private Panel addTab(final TabPanel tabPanel, int index) {
+         Panel tab = new Panel();
+         tab.setAutoScroll(true);
+         tab.setTitle("New Tab " + (++index));
+         tab.setIconCls("tab-icon");
+         tab.setHtml("Tab Body " + index + "<br/><br/>" );  
+         tab.setClosable(true);
+         tabPanel.add(tab);
+         return tab;
+     }
+
+
+    private void createAndAddWestPanel(Panel borderPanel)
+    {
+        Panel westPanel = new Panel();
+        westPanel.setHtml("<p>west panel</p>");
+        westPanel.setTitle("West");
+        westPanel.setBodyStyle("background-color: EEEEEE");
+        westPanel.setCollapsible(true);
+        westPanel.setWidth(200);
+
+        BorderLayoutData westData = new BorderLayoutData(RegionPosition.WEST);
+        westData.setSplit(true);
+        westData.setMinSize(175);
+        westData.setMaxSize(400);
+        westData.setMargins(new Margins(0, 5, 0, 0));
+
+
+        final TreePanel treePanel = new SampleTree();
+        treePanel.setTitle("Default Appearance");
+        treePanel.setWidth(190);
+        treePanel.setHeight(400);
+
+
+        borderPanel.add(treePanel, westData);
+    }
+
+
+       class SampleTree extends TreePanel {
+
+           public SampleTree() {
+
+               TreeNode root = new TreeNode("Company Heirarchy");
+
+               TreeNode ceo = new TreeNode("Julie W. Walker");
+               ceo.setExpanded(true);
+
+               TreeNode manager1 = new TreeNode("William J. Vear");
+               manager1.setExpanded(true);
+               TreeNode manager2 = new TreeNode("Dennis E. Walker");  
+               manager2.setExpanded(true);
+               TreeNode manager3 = new TreeNode("Joann R. Williams");
+               manager3.setExpanded(true);
+
+               ceo.appendChild(manager1);
+               ceo.appendChild(manager2);
+               ceo.appendChild(manager3);
+
+               TreeNode director1 = new TreeNode("Robert L. Carbaugh");
+               director1.setExpanded(true);
+
+               TreeNode director2 = new TreeNode("Agnes H. Keene");
+               director2.setExpanded(true);
+
+               manager1.appendChild(director1);
+               manager1.appendChild(director2);
+
+               TreeNode director3 = new TreeNode("Erin T. Marks");
+               manager2.appendChild(director3);
+
+               manager3.appendChild(new TreeNode("Harry L. Krieger"));
+
+               director1.appendChild(new TreeNode("Jim H. Baker"));
+               director1.appendChild(new TreeNode("Randy M. Smith"));
+               director1.appendChild(new TreeNode("Annie P. Burke"));
+               director2.appendChild(new TreeNode("Shirley P. Tanaka"));
+               director2.appendChild(new TreeNode("Anthony C. Decarlo"));
+               director2.appendChild(new TreeNode("Katherine D. Saenz"));
+               director3.appendChild(new TreeNode("Carolyn M. Gauna"));
+               director3.appendChild(new TreeNode("Johanna E. Armistead"));
+               director3.appendChild(new TreeNode("Duane E. Ashe"));
+               director3.appendChild(new TreeNode("Norman N. Gardner"));
+               root.appendChild(ceo);
+
+               setRootVisible(false);
+
+               setTitle("Company");
+               setWidth(200);
+               setHeight(400);
+               setRootNode(root);
+               root.setExpanded(true);
+           }
+       }
+
 
 }
 
--- trunk/modules/presentation/gweb/src/main/java/org/grouter/presentation/gwt/server/GWTRemoteServiceImpl.java
+++ trunk/modules/presentation/gweb/src/main/java/org/grouter/presentation/gwt/server/GWTRemoteServiceImpl.java
@@ -1,11 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.grouter.presentation.gwt.server;
 
 import org.grouter.domain.entities.Message;
+import org.grouter.domain.service.RouterService;
 import org.grouter.presentation.gwt.client.GWTRemoteService;
 import org.gwtwidgets.server.spring.GWTSpringController;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -16,6 +35,7 @@ import java.util.ArrayList;
 //public class GWTRemoteServiceImpl     extends RemoteServiceServlet implements GWTRemoteService
 public class GWTRemoteServiceImpl extends GWTSpringController implements GWTRemoteService
 {
+    private RouterService routerService;
     public String getMessages(Integer nodeId)
     {
         List<Message> messages = new ArrayList<Message>();
@@ -29,7 +49,8 @@ public class GWTRemoteServiceImpl extends GWTSpringController implements GWTRemo
         return "messages";
     }
 
-    public void setRouterService(String routerService)
+    public void setRouterService(RouterService routerService)
     {
+        this.routerService = routerService;
     }
 }
